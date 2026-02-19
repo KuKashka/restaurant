f085ae0abfcf88960c160c66978d7dae625436c4
KuKashka <ostovrah@gmail.com>
2026-02-19 20:30:26 +0200

    Головна сторінка є

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from main.models import Category
+def home(request):
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+
+    return render(request, 'home.html', context)
+
 # Create your views here.
