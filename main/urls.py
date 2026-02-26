566621dd7d46dd3039a0c7c8b033dc1884640770
KuKashka <ostovrah@gmail.com>
2026-02-26 20:27:25 +0200

    Готовий кошик

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     # Визнач тут свої URL-шляхи
     path('', views.home, name='home'),
+    path('add_to_order/<int:dish_id>/', views.add_to_order, name='add_to_order'),
+    path('cart/', views.view_cart, name='view_cart'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-from main.models import Category
+from main.models import Category, Dish, Order, DishInOrder
 def home(request):
     categories = Category.objects.all()
     context = {
@@ -8,5 +8,38 @@ def home(request):
     }
 
     return render(request, 'home.html', context)
+
+def add_to_order(request, dish_id):
+    # Логіка для додавання страви до замовлення
+    if request.method == 'POST':
+        # Тут можна реалізувати логіку додавання страви до замовлення
+        dish = Dish.objects.get(id=dish_id)
+        cart = request.session.get('cart', {})
+        if str(dish_id) in cart:
+            cart[str(dish_id)] += 1
+        else:
+            cart[str(dish_id)] = 1
+        request.session['cart'] = cart
+    return redirect('home')
+
+def view_cart(request):
+    cart = request.session.get('cart', {})
+    dishes_in_cart = []
+    total_price = 0
+
+    for dish_id, quantity in cart.items():
+        dish = Dish.objects.get(id=dish_id)
+        dishes_in_cart.append({
+            'dish': dish,
+            'quantity': quantity,
+            'total_price': dish.price * quantity
+        })
+        total_price += dish.price * quantity
+
+    context = {
+        'dishes_in_cart': dishes_in_cart,
+        'total_price': total_price
+    }
+    return render(request, 'cart.html', context)
 
 # Create your views here.
